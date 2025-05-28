20333e269a851daf3d4e2e923b16434f49157814
Mailton Lemos <lemos.dark@outlook.com>
2025-05-28 09:10:23 -0300

    inicializacao

---
+++ src/main/java/com/mycompany/sistemabibliotecario/SistemaBibliotecario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemabibliotecario;
+
+/**
+ *
+ * @author mailton
+ */
+public class SistemaBibliotecario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
